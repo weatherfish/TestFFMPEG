83993be2d6c3152bd97f94cca50c8d2cfe7562cd
Felix <weatherfish2010@gmal.com>
2024-06-17 21:38:55 +0800

    1

--- main.cpp
+++ main.cpp
@@ -12,12 +12,21 @@ void stopVideo(ReSample* sample){
 
 int main(int argc, char** argv){
     // ReSample *sample = new ReSample("sample.mp4", "video.yuv");
-    ReSample *sample = new ReSample("0", "video.yuv");
+    // ReSample *sample = new ReSample("0", "video.yuv");
     
     // std::thread t(stopVideo, std::ref(sample));
-    sample->startRecording();
+    // sample->startRecording();
     // t.join();
     // free(sample);
+
+    AVGeneralMediaInfo *info = new AVGeneralMediaInfo();
+    if(info){
+        get_avgeneral_mediainfo(info, "ample.mp4");
+
+        
+        delete info;
+        info = NULL;
+    }
 
     return 0; // Successful execution
 }
