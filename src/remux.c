6b5a15b08ea034050fe9c3fcf6b0684d3251e6cf
Felix Wang <felix@aopacloud.sg>
2024-06-30 12:18:17 +0800

    remux done

--- src/remux.c
+++ src/remux.c
@@ -8,10 +8,8 @@ int remux(int argc, char *argv[]) {
     char *dst;
 
     int ret = -1;
-    int videoIndex = -1;
     AVFormatContext *formatContext = NULL;
     AVFormatContext *dstFormatContext = NULL;
-    const AVOutputFormat *outFormat = NULL;
     AVPacket packet;
 
     int *streamMap = NULL;
@@ -35,32 +33,30 @@ int remux(int argc, char *argv[]) {
 
     // 4. 打开目标文件上下文
     avformat_alloc_output_context2(&dstFormatContext, NULL, NULL, dst);
-    if(!dstFormatContext){
+    if (!dstFormatContext) {
         av_log(NULL, AV_LOG_ERROR, "内存不足\n");
         goto _ERROR;
     }
 
     streamMap = av_calloc(formatContext->nb_streams, sizeof(int));
-    if(!streamMap){
+    if (!streamMap) {
         av_log(NULL, AV_LOG_ERROR, "内存不足\n");
         goto _ERROR;
     }
-    for (int i = 0; i < formatContext->nb_streams; i++)
-    {
+    for (int i = 0; i < formatContext->nb_streams; i++) {
         AVStream *inStream = formatContext->streams[i];
         AVCodecParameters *params = inStream->codecpar;
-        if(params->codec_type != AVMEDIA_TYPE_AUDIO&&
-        params->codec_type != AVMEDIA_TYPE_VIDEO&&
-        params->codec_type != AVMEDIA_TYPE_SUBTITLE
-        ){
+        if (params->codec_type != AVMEDIA_TYPE_AUDIO &&
+            params->codec_type != AVMEDIA_TYPE_VIDEO &&
+            params->codec_type != AVMEDIA_TYPE_SUBTITLE) {
             streamMap[i] = -1;
             continue;
         }
-        streamMap[i] = stream_index++; 
-        
+        streamMap[i] = stream_index++;
+
         // 5. 为目标文件创建新的视频流
         AVStream *outStream = avformat_new_stream(dstFormatContext, NULL);
-        if(!outStream){
+        if (!outStream) {
             av_log(dstFormatContext, AV_LOG_ERROR, "内存不足\n");
             goto _ERROR;
         }
@@ -68,7 +64,7 @@ int remux(int argc, char *argv[]) {
         avcodec_parameters_copy(outStream->codecpar, inStream->codecpar);
         outStream->codecpar->codec_tag = 0; // 根据多媒体文件自动适配编解码器
     }
-    
+
     // 绑定上下文
     ret = avio_open2(&dstFormatContext->pb, dst, AVIO_FLAG_WRITE, NULL, NULL);
     if (ret < 0) {
@@ -88,12 +84,12 @@ int remux(int argc, char *argv[]) {
         AVStream *inStream, *outStream;
         inStream = formatContext->streams[packet.stream_index];
 
-        if(packet.stream_index < 0){
+        if (packet.stream_index < 0) {
             av_packet_unref(&packet);
             continue;
         }
         packet.stream_index = streamMap[packet.stream_index];
-        outStream = dstFormatContext ->streams[packet.stream_index];
+        outStream = dstFormatContext->streams[packet.stream_index];
         av_packet_rescale_ts(&packet, inStream->time_base, outStream->time_base);
         packet.pos = -1; // 相对位置，内部自行计算
         av_interleaved_write_frame(dstFormatContext, &packet);
@@ -116,7 +112,7 @@ _ERROR:
         avformat_free_context(dstFormatContext);
         dstFormatContext = NULL;
     }
-    if(streamMap){
+    if (streamMap) {
         av_free(streamMap);
         streamMap = NULL;
     }
